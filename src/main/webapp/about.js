79490620a3c176e5bb6009109d5eae1ec1685fe4
RAC3R <stefanos_aris@hotmail.com>
2015-11-22 17:55:07 +0200

    na mai ki egw closed #18

---
+++ src/main/webapp/about.js
@@ -0,0 +1,7 @@
+/**
+ * @author Stef
+ */
+function showDetails()
+		{
+			alert("Welcome to SnapChaty. Here you can chat with strangers inside a certain range. Your messages auto-delete after a few seconds.")
+		}
