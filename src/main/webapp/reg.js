8c2747f770ca81049230d891f97a818818edf0a7
Stamos <stamosrep@gmail.com>
2015-11-24 02:46:32 +0200

    The client can sent sign-up data. close #4
    Provides to the client the ability to send sign-up data to the server
    and observe the result.

--- src/main/webapp/reg.js
+++ src/main/webapp/reg.js
@@ -1,35 +1,75 @@
+var request = false;
 
-var req = false;
-
-function test(){
-
+function registerUser() {
 	var firstName = document.getElementById("firstName").value;
+	
 	var lastName = document.getElementById("lastName").value;
+	
 	var username = document.getElementById("username").value;
+	
 	var password = document.getElementById("password").value;
+	
+	var repeatedPassword = document.getElementById("repeatedPassword").value;
+	
 	var email = document.getElementById("email").value;
-	if(firstName && lastName && username && password && email){
+	
+	if (firstName && lastName && username && password && email && password == repeteadPassword) {
 		var url = "http://localhost:8080/SnapChatyX/webapi/signup";
-		var dataToSend = ("firstName:" + firstName + ",lastName:" + lastName + ",username:" + username + ",password:" + password + ",email:" + email);
-		if(window.XMLHttpRequest){
-			req = new XMLHttpRequest();
-		}
-		else{
-			if(window.ActiveXObject){
-				try{
-					req = new ActiveXObject("Microsoft.XMLHTTP");
+		
+		var dataToSend = JSON.stringify({
+			"firstName" : firstName,
+			"lastName" : lastName,
+			"username" : username,
+			"password" : password.toString(),
+			"email" : email
+		});
+		
+		if (window.XMLHttpRequest) {
+			request = new XMLHttpRequest();
+		} else {
+			if (window.ActiveXObject) {
+				try {
+					request = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (ex) {
+					alert("Error creating request" + ex.toString());
 				}
-				catch (e){alert("Error creating request" + e.toString())}
 			}
 		}
-		if(req){
-			req.open("POST", url, true);
-			req.setRequestHeader("Content-Type", "text/plain");
-			req.setRequestHeader("Content-length", dataToSend.length);
-			req.send(dataToSend);
+		if (request) {
+			request.open("POST", url, true);
+			
+			request.setRequestHeader("Content-Type", "text/plain");
+			
+			request.setRequestHeader("Content-length", dataToSend.length);
+			
+			request.send(dataToSend);
+			
+			request.onreadystatechange = handleResponse(); 
+		} else {
+			alert("REQUEST FAILED.");
 		}
-		else alert("REQUEST FAILED.");
-	
+	} else {
+		alert("YOU DIDNT SET DATA.");
 	}
-	else alert("YOU DIDNT SET DATA");
+}
+
+/**
+ * Handles the response which the server returns.
+ */
+function handleResponse() {
+	var responseText = request.responseXML.responseText;
+
+	if (request.readyState == 4 && request.status == 200)
+	{
+		alert(
+				"You have signed up successfully!" + "\n" +
+				"* " + responseText + " *"
+		);
+	} else {
+		alert(
+				"An error occurred while trying to sign up..." + "\n" +
+				"* " + responseText + " *"
+
+		);
+	}
 }
