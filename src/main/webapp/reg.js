6e9f2f97d056b0878ff2b29de1c6a4a5e75a2add
Stamos <stamosrep@gmail.com>
2015-11-25 20:16:01 +0200

    Completed Story 1.

--- src/main/webapp/reg.js
+++ src/main/webapp/reg.js
@@ -2,27 +2,30 @@ var request = false;
 
 function registerUser() {
 	var firstName = document.getElementById("firstName").value;
-	
-	var lastName = document.getElementById("lastName").value;
-	
-	var username = document.getElementById("username").value;
-	
-	var password = document.getElementById("password").value;
+
+    var lastName = document.getElementById("lastName").value;
+
+    var username = document.getElementById("username").value;
+
+    var password = document.getElementById("password").value;
 	
 	var repeatedPassword = document.getElementById("repeatedPassword").value;
+
+    var email = document.getElementById("email").value;
 	
-	var email = document.getElementById("email").value;
+	var userValidation =
+		new UserValidation(firstName, lastName, username, password, repeatedPassword, email);
 	
-	if (firstName && lastName && username && password && email && password == repeteadPassword) {
-		var url = "http://localhost:8080/SnapChatyX/webapi/signup";
-		
-		var dataToSend = JSON.stringify({
+	if (userValidation.isValid()) {
+		var userStringAsJSON = JSON.stringify({
 			"firstName" : firstName,
 			"lastName" : lastName,
 			"username" : username,
 			"password" : password.toString(),
 			"email" : email
 		});
+		
+		var url = "http://localhost:8080/SnapChatyX/webapi/signup";
 		
 		if (window.XMLHttpRequest) {
 			request = new XMLHttpRequest();
@@ -40,16 +43,18 @@ function registerUser() {
 			
 			request.setRequestHeader("Content-Type", "text/plain");
 			
-			request.setRequestHeader("Content-length", dataToSend.length);
+			request.setRequestHeader("Content-length", userStringAsJSON.length);
 			
-			request.send(dataToSend);
+			request.send(userStringAsJSON);
 			
-			request.onreadystatechange = handleResponse(); 
+			request.onreadystatechange = handleResponse();
 		} else {
-			alert("REQUEST FAILED.");
+			alert("Request failed!");
 		}
 	} else {
-		alert("YOU DIDNT SET DATA.");
+		alert(
+				"The data you iserted are not valid!"
+		);
 	}
 }
 
@@ -57,7 +62,7 @@ function registerUser() {
  * Handles the response which the server returns.
  */
 function handleResponse() {
-	var responseText = request.responseXML.responseText;
+	var responseText = request.responseText;
 
 	if (request.readyState == 4 && request.status == 200)
 	{
@@ -69,7 +74,100 @@ function handleResponse() {
 		alert(
 				"An error occurred while trying to sign up..." + "\n" +
 				"* " + responseText + " *"
-
 		);
 	}
+}
+
+var UserValidation = function(firstName, lastName, username, password, repeatedPassword, email) {
+	this.firstName = firstName;
+	
+	this.lastName = lastName;
+	
+	this.username = username;
+	
+	this.password = password;
+	
+	this.repeatedPassword = repeatedPassword;
+	
+	this.email = email;
+}
+
+UserValidation.prototype.isValid = function() {
+	var userIsValid = false;
+	
+	if (
+		this.isFirstNameValid() &&
+		this.isLastNameValid() &&
+		this.isUsernameValid() &&
+		this.isPasswordValid() &&
+		this.isEmailValid()
+	) {
+		userIsValid = true;
+	}
+	
+	return userIsValid;
+}
+
+UserValidation.prototype.isFirstNameValid = function() {
+	var userFirstNameIsValid = false;
+	
+	if (!(this.firstName == null || this.firstName == "")) {
+		userFirstNameIsValid = true;
+	} else {
+		document.getElementById("firstName").value = null;
+	}
+	
+	return userFirstNameIsValid;
+}
+
+UserValidation.prototype.isLastNameValid = function() {
+	var userLastNameIsValid = false;
+	
+	if (!(this.lastName == null || this.lastName == "")) {
+		userLastNameIsValid = true;
+	} else {
+		document.getElementById("lastName").value = null;
+	}
+	
+	return userLastNameIsValid;
+}
+
+UserValidation.prototype.isUsernameValid = function() {
+	var userUsernameIsValid = false;
+	
+	if (!(this.username == null || this.username == "")) {
+		userUsernameIsValid = true;
+	} else {
+		document.getElementById("username").value = null;
+	}
+	
+	return userUsernameIsValid;
+}
+
+UserValidation.prototype.isPasswordValid = function() {
+	var userPasswordIsValid = false;
+	
+	if (
+		!(this.password == null || this.password == "") &&
+		this.password == this.repeatedPassword
+	) {
+		userPasswordIsValid = true;
+	} else {
+		document.getElementById("password").value = null;
+		document.getElementById("repeatedPassword").value = null;
+	}
+	
+	return userPasswordIsValid;
+}
+
+UserValidation.prototype.isEmailValid = function() {
+	var userEmailIsValid = false;
+	
+	if (!(this.email == null || this.email == "")) {
+		userEmailIsValid = true;
+	} else {
+		document.getElementById("password").value = null;
+	}
+	
+	return userEmailIsValid;
 }
