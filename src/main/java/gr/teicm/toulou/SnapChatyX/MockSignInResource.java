992560132a5378f8b924c61251cc8516d1067540
Orestis <orestispaok@windowslive.com>
2015-11-25 20:53:08 +0200

    mockResource

---
+++ src/main/java/gr/teicm/toulou/SnapChatyX/MockSignInResource.java
@@ -0,0 +1,21 @@
+package gr.teicm.toulou.SnapChatyX;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("signin")
+public class MockSignInResource {
+	
+	@POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response signUp(String user) {
+    	 System.out.println(user);
+	     String result = "You signed in successfully";   
+    	 return Response.status(200).entity(result).build();
+	}
+}
