43b4c30bdb91b482fff913e8cd6a92b4bb782ec2
Stamos <stamosrep@gmail.com>
2015-12-17 17:27:30 +0200

    refactor
    Refactor on useless architecure

--- src/main/java/gr/teicm/toulou/SnapChatyX/services/UserService.java
+++ src/main/java/gr/teicm/toulou/SnapChatyX/services/UserService.java
@@ -1,69 +1,59 @@
 package gr.teicm.toulou.SnapChatyX.services;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import gr.teicm.toulou.SnapChatyX.factories.RepositoryFactory;
+import gr.teicm.toulou.SnapChatyX.model.IUserHistory;
 import gr.teicm.toulou.SnapChatyX.model.UserHistory;
-import gr.teicm.toulou.SnapChatyX.repositories.IRepository;
+import gr.teicm.toulou.SnapChatyX.repositories.IUserHistoryRepository;
 import gr.teicm.toulou.SnapChatyX.repositories.UserHistoryRepository;
 
 /**
+ * 
+ * 
  * @since Dec 8, 2015
  * 
  * @author Stamatios Tsalikis
- * 
  */
 public class UserService {
 	
 	private static UserService instance;
 	
-	private final Map<IRepository.RepositoryType, IRepository> repositoryMap;
+	private final IUserHistoryRepository userHistoryRepository;
 	
 	private UserService() {
-		repositoryMap = new HashMap<>();
 		
-		try {
-			repositoryMap.put(
-					IRepository.RepositoryType.USER_HISTORY,
-					RepositoryFactory.getInstance(UserHistoryRepository.class)
-			);
-		} catch (InstantiationException ex) {
-			ex.printStackTrace();
-		} catch (IllegalAccessException ex) {
-			ex.printStackTrace();
-		}
+		userHistoryRepository = new UserHistoryRepository();
+		
 	}
 	
 	public static UserService getInstance() {
+		
 		if (instance.equals(null)) {
+			
 			instance = new UserService();
+			
 		}
 		
 		return instance;
-	}
-	
-	public void addUserHistory(UserHistory userHistory) {
-		repositoryMap.get(IRepository.RepositoryType.USER_HISTORY).addElement(userHistory);
-	}
-	
-	public UserHistory getUserHistoryByUsername(String username) {
-		UserHistory userHistory =
-				(UserHistory) repositoryMap
-				.get(IRepository.RepositoryType.USER_HISTORY)
-				.findElementByUsername(username);
 		
-		return userHistory;
 	}
 	
-	public List<UserHistory> findAllUserHistories() {
-		List<UserHistory> userHistoryList =
-				(List<UserHistory>) repositoryMap
-				.get(IRepository.RepositoryType.USER_HISTORY)
-				.findAllElements();
+	public void setUserHistoryByUsername(UserHistory userHistory) {
 		
-		return userHistoryList;
+		userHistoryRepository.setUserHistoryByUsername(userHistory);
+		
+	}
+	
+	public IUserHistory getUserHistoryByUsername(String username) {
+		
+		return userHistoryRepository.findUserHistoryByUsername(username);
+		
+	}
+	
+	public List<IUserHistory> findAllUserHistories() {
+		
+		return userHistoryRepository.findAllUserHistories();
+		
 	}
 	
 }
