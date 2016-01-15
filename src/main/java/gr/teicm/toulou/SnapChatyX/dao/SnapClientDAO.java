7b5e9c9424adac74ed6389c24dea19d95462a2b5
Orestis <orestispaok@windowslive.com>
2016-01-15 17:39:01 +0200

    SnapClientDAOTest

--- src/main/java/gr/teicm/toulou/SnapChatyX/dao/SnapClientDAO.java
+++ src/main/java/gr/teicm/toulou/SnapChatyX/dao/SnapClientDAO.java
@@ -10,9 +10,18 @@ public class SnapClientDAO extends SnapBasicDAO<SnapClientEntity, String> {
 		
 	}
 	
-	public void createSnapClientEntity(SnapClientEntity entity) {
+	public boolean createSnapClientEntity(SnapClientEntity entity) {
 		
-		getDatastore().save(entity);
+		if(null == entity) {
+			throw new IllegalArgumentException("Param must not be null.");
+		}
+		
+		if(null == getSnapClientEntityById(entity.getId())) {
+			getDatastore().save(entity);
+			return true;
+		}
+		
+		return false;
 		
 	}
 	
@@ -22,8 +31,8 @@ public class SnapClientDAO extends SnapBasicDAO<SnapClientEntity, String> {
 		
 	}
 	
-	public void updateSnapClientEntity(SnapClientEntity entity) {
-		//FIXME:2nd .set doesn't work! 
+	public void updateSnapClientFriendList(SnapClientEntity entity) {
+
 		getDatastore().update(
 				getDatastore()
 					.createQuery(SnapClientEntity.class)
@@ -32,7 +41,29 @@ public class SnapClientDAO extends SnapBasicDAO<SnapClientEntity, String> {
 				getDatastore()
 					.createUpdateOperations(SnapClientEntity.class)
 					.set("friendList", entity.getFriendList())	
+					
 		);		
+	}
+	
+	public void updateSnapClientBlackList(SnapClientEntity entity) {
+		 
+		getDatastore().update(
+				getDatastore()
+					.createQuery(SnapClientEntity.class)
+					.field("_id")
+					.equal(entity.getId()),
+				getDatastore()
+					.createUpdateOperations(SnapClientEntity.class)
+					.set("blackList", entity.getBlackList())
+					
+		);		
+	}
+	
+	public void updateSnapClientEntity(SnapClientEntity entity) {
+		
+		updateSnapClientFriendList(entity); 
+		updateSnapClientBlackList(entity); 
+		
 	}
 	
 	public void deleteSnapClientEntity(SnapClientEntity entity) {
